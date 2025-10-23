d67d90f6457b4ff8c59453a637f3ff6ae712e1c7
RaneKup <egorushka230512@gmail.com>
2025-10-23 19:16:14 +0700

    Update prog.py
    изменение кода

--- prog.py
+++ prog.py
@@ -5,19 +5,8 @@ import random
 
 nums = []
 
-random1 = random.randint(1, 24)
-random2 = random.randint(1, 24)
-random3 = random.randint(1, 24)
-random4 = random.randint(1, 24)
-random5 = random.randint(1, 24)
-random6 = random.randint(1, 24)
-
-nums.append(random1)
-nums.append(random2)
-nums.append(random3)
-nums.append(random4)
-nums.append(random5)
-nums.append(random6)
+for i in range(6):
+    nums.append(random.randint(1,24))
 
 count = 0
 for i in nums:
